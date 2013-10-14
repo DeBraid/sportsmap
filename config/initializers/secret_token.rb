c07db7712aaac2160199bd024a89c8afd2f29126
DeBraid <de.braid@gmail.com>
2013-10-14 13:32:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sportsmap::Application.config.secret_key_base = 'aacf8c33071e31fdf00d8f565c9eeb79844464b0bcfd0668efd01504a56cdfee0f5b54e996c7fb84c7d0fc57ddff2c172a9b7276a7477e13f139e4f2f6984ed5'
