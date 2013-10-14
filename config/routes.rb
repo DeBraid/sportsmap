559d51429d8efa97b14ec2db24b412d28a37d8ab
DeBraid <de.braid@gmail.com>
2013-10-14 13:48:51 -0400

    created maps controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Sportsmap::Application.routes.draw do
+
+  get "maps/index"
+  get 'maps' => 'maps#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
